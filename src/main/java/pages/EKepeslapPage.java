7be4e82907c7b1929310ec8b210cb5a492295e70
benciitoo <bence.banszegi@gmail.com>
2019-05-24 01:43:01 +0200

    refactor(EKepeslapPage): code styling

--- src/main/java/pages/EKepeslapPage.java
+++ src/main/java/pages/EKepeslapPage.java
@@ -14,9 +14,11 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.HashMap;
 import java.util.Map;
 
-public class PostcardSender {
+public class EKepeslapPage {
 
-    private static final String[] emojis = new String[] {
+
+    private static final String URL = "https://www.e-kepeslap.com/kepeslapkuldes-13-szuletesnapi-kepeslapok.html";
+    private static final String[] EMOJIS = new String[] {
             " (_mosoly_) ", " (_nevet_) ", " (_szomoru_) ", " (_kacsint_) ", " (_nyelv_) ",
             " (_csodalkozo_) ", " (_sir_) ", " (_szemuveg_) ", " (_alszom_) ", " (_szarkasztikus_) ",
             " (_szegyenlos_) ", " (_nemtudom_) ", " (_lakat_) ", " (_beteg_) ", " (_duhos_) ",
@@ -28,28 +30,28 @@ public class PostcardSender {
             " (_ajandek2_) ", " (_udito_) ", " (_koktel_) ", " (_koktel2_) ", " (_sor1_) ",
             " (_sor2_) ", " (_bor_) ",
     };
-    private static final By quoteMenu = By.id("Idezetek");
-    private static final By postcardTitle = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[2]/input");
-    private static final By postcardBody = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[3]/textarea");
-    private static final By signatureField = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[5]/input");
-    private static final By receiverNameField = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[6]/input");
-    private static final By receiverEmailField = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[7]/input");
-    private static final By ownNameField = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[8]/input");
-    private static final By ownEmailField = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[9]/input");
-    private static final By sendPostcardButton = By.xpath("/html/body/div[2]/ul/li[1]/form/input[21]");
-    private static final By postcardFontColorMenu = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[10]/div[1]/label/select");
-    private static final By postcardFontTypeMenu = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[11]/div[1]/label/select");
-    private static final By postcardFontSizeMenu = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[12]/div/label/select");
-    private static final By postcardBackgroundColorMenu = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[13]/div[1]/label/select");
-    private static final By postcardStampMenu = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[14]/div[1]/label/select");
-    private static final By postcardBackgroundMenu = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[15]/div[1]/label/select");
-    private static final By postcardBackgroundSong = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[17]/div/label/select");
+    private static final By QUOTE_MENU = By.id("Idezetek");
+    private static final By POSTCARD_TITLE = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[2]/input");
+    private static final By POSTCARD_BODY = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[3]/textarea");
+    private static final By SIGNATURE_FIELD = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[5]/input");
+    private static final By RECEIVER_NAME_FIELD = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[6]/input");
+    private static final By RECEIVER_EMAIL_FIELD = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[7]/input");
+    private static final By OWN_NAME_FIELD = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[8]/input");
+    private static final By OWN_EMAIL_FIELD = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[9]/input");
+    private static final By SEND_POSTCARD_BUTTON = By.xpath("/html/body/div[2]/ul/li[1]/form/input[21]");
+    private static final By POSTCARD_FONT_COLOR_MENU = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[10]/div[1]/label/select");
+    private static final By POSTCARD_FONT_TYPE_MENU = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[11]/div[1]/label/select");
+    private static final By POSTCARD_FONT_SIZE_MENU = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[12]/div/label/select");
+    private static final By POSTCARD_BACKGROUND_COLOR_MENU = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[13]/div[1]/label/select");
+    private static final By POSTCARD_STAMP_MENU = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[14]/div[1]/label/select");
+    private static final By POSTCARD_BACKGROUND_MENU = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[15]/div[1]/label/select");
+    private static final By POSTCARD_BACKGROUND_SONG = By.xpath("//*[@id=\"tartalom\"]/table/tbody/tr/td[1]/form/div/div[17]/div/label/select");
 
     private static WebDriver driver;
     private static Map<String, String> config = new HashMap<>();
 
-    PostcardSender(WebDriver extDriver, String postcardTitle, String postcardBodyMessage, String signature,
-              String receiverUsername, String receiverEmail, String ownUsername, String ownEmail) {
+    EKepeslapPage(WebDriver extDriver, String postcardTitle, String postcardBodyMessage, String signature,
+                  String receiverUsername, String receiverEmail, String ownUsername, String ownEmail) {
 
         /*
         postcardBodyMessage can contain emojis:
@@ -64,7 +66,7 @@ public class PostcardSender {
         driver = extDriver;
         PropertyUtility.getPropertyFileContent(config);
         driver = DriverUtility.startUp(driver, config, false, true, true);
-        driver.get("https://www.e-kepeslap.com/kepeslapkuldes-13-szuletesnapi-kepeslapok.html");
+        driver.get(URL);
         ((JavascriptExecutor) driver)
                 .executeScript(String.format(
                         "document.querySelector(\"#tartalom > table > tbody > tr > td.fo > div > ul > li:nth-child(%s) > a > img\").click()",
@@ -86,72 +88,71 @@ public class PostcardSender {
         selectPostcardSong();
         submitPostcard();
         sendPostcard();
-
     }
 
     private static void fillPostcardTitle(String text) {
         new WebDriverWait(driver, GlobalVariables.GENERAL_EXPLICIT_TIMEOUT)
-                .until(ExpectedConditions.presenceOfElementLocated(postcardTitle)).sendKeys(text);
+                .until(ExpectedConditions.presenceOfElementLocated(POSTCARD_TITLE)).sendKeys(text);
     }
 
     private static void fillPostcardBody(String text) {
         text = encodeEmojis(text);
         new WebDriverWait(driver, GlobalVariables.GENERAL_EXPLICIT_TIMEOUT)
-                .until(ExpectedConditions.presenceOfElementLocated(postcardBody)).sendKeys(text);
+                .until(ExpectedConditions.presenceOfElementLocated(POSTCARD_BODY)).sendKeys(text);
     }
 
     private static void fillSignature(String text) {
         new WebDriverWait(driver, GlobalVariables.GENERAL_EXPLICIT_TIMEOUT)
-                .until(ExpectedConditions.presenceOfElementLocated(signatureField)).sendKeys(text);
+                .until(ExpectedConditions.presenceOfElementLocated(SIGNATURE_FIELD)).sendKeys(text);
     }
 
     private static void fillReceiverData(String name, String email) {
         new WebDriverWait(driver, GlobalVariables.GENERAL_EXPLICIT_TIMEOUT)
-                .until(ExpectedConditions.presenceOfElementLocated(receiverNameField)).sendKeys(name);
+                .until(ExpectedConditions.presenceOfElementLocated(RECEIVER_NAME_FIELD)).sendKeys(name);
         new WebDriverWait(driver, GlobalVariables.GENERAL_EXPLICIT_TIMEOUT)
-                .until(ExpectedConditions.presenceOfElementLocated(receiverEmailField)).sendKeys(email);
+                .until(ExpectedConditions.presenceOfElementLocated(RECEIVER_EMAIL_FIELD)).sendKeys(email);
     }
 
     private static void fillOwnData(String name, String email) {
         new WebDriverWait(driver, GlobalVariables.GENERAL_EXPLICIT_TIMEOUT)
-                .until(ExpectedConditions.presenceOfElementLocated(ownNameField)).sendKeys(name);
+                .until(ExpectedConditions.presenceOfElementLocated(OWN_NAME_FIELD)).sendKeys(name);
         new WebDriverWait(driver, GlobalVariables.GENERAL_EXPLICIT_TIMEOUT)
-                .until(ExpectedConditions.presenceOfElementLocated(ownEmailField)).sendKeys(email);
+                .until(ExpectedConditions.presenceOfElementLocated(OWN_EMAIL_FIELD)).sendKeys(email);
     }
 
     private static void selectQuote() {
         ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, -2000)");
-        selectFromMenu(quoteMenu, 48);
+        selectFromMenu(QUOTE_MENU, 48);
     }
 
     private static void selectPostcardFontColor() {
         ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, -500)");
-        selectFromMenu(postcardFontColorMenu, 61);
+        selectFromMenu(POSTCARD_FONT_COLOR_MENU, 61);
     }
 
     private static void selectPostcardFontType() {
-        selectFromMenu(postcardFontTypeMenu, 16);
+        selectFromMenu(POSTCARD_FONT_TYPE_MENU, 16);
     }
 
     private static void selectPostcardFontSize() {
-        selectFromMenu(postcardFontSizeMenu, 3);
+        selectFromMenu(POSTCARD_FONT_SIZE_MENU, 3);
     }
 
     private static void selectPostcardBackgroundColor() {
-        selectFromMenu(postcardBackgroundColorMenu, 61);
+        selectFromMenu(POSTCARD_BACKGROUND_COLOR_MENU, 61);
     }
 
     private static void selectPostcardStamp() {
-        selectFromMenu(postcardStampMenu, 44);
+        selectFromMenu(POSTCARD_STAMP_MENU, 44);
     }
 
     private static void selectPostcardBackground() {
-        selectFromMenu(postcardBackgroundMenu, 129);
+        selectFromMenu(POSTCARD_BACKGROUND_MENU, 129);
     }
 
     private static void selectPostcardSong() {
         ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, -500)");
-        selectFromMenu(postcardBackgroundSong, 64);
+        selectFromMenu(POSTCARD_BACKGROUND_SONG, 64);
     }
 
     private static void submitPostcard() {
@@ -163,8 +164,8 @@ public class PostcardSender {
         new WebDriverWait(driver, GlobalVariables.GENERAL_EXPLICIT_TIMEOUT)
                 .until(
                         ExpectedConditions.and(
-                                ExpectedConditions.presenceOfElementLocated(sendPostcardButton),
-                                ExpectedConditions.elementToBeClickable(sendPostcardButton)
+                                ExpectedConditions.presenceOfElementLocated(SEND_POSTCARD_BUTTON),
+                                ExpectedConditions.elementToBeClickable(SEND_POSTCARD_BUTTON)
                         )
                 );
         ((JavascriptExecutor) driver)
@@ -189,7 +190,7 @@ public class PostcardSender {
             try {
                 emojiCode = Integer.parseInt(charCodes);
                 if (emojiCode > -1 && emojiCode < 48) {
-                    text = text.substring(0, startCodeIndex - 1) + emojis[emojiCode] + text.substring(charCodeLength);
+                    text = text.substring(0, startCodeIndex - 1) + EMOJIS[emojiCode] + text.substring(charCodeLength);
                 }
             }
             catch (NumberFormatException e) {
@@ -219,5 +220,4 @@ public class PostcardSender {
             }
         }
     }
-
 }
