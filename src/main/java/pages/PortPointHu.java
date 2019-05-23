5017c730afc841cc96576c5b319d00305e3a6818
Varga Balint <vargabalint@gmail.com>
2019-05-23 13:41:02 +0200

    feat: add programtype chooser

--- src/main/java/pages/PortPointHu.java
+++ src/main/java/pages/PortPointHu.java
@@ -1,4 +1,42 @@
 package pages;
 
+import commons.GlobalVariables;
+import enums.PortProgramType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
 public class PortPointHu {
+
+    private WebDriverWait wait;
+    private WebDriver driver;
+    private By programSearchFrom = By.id("top-search-form");
+    private By programType = By.xpath("//form[@id='top-search-form']/ul/li[3]/a/span");
+    private By searchProgramButton = By.xpath("//form[@id='top-search-form']/button");
+
+
+    public PortPointHu(WebDriver driver) {
+        this.wait = new WebDriverWait(driver, GlobalVariables.GENERAL_EXPLICIT_TIMEOUT);
+        this.driver = driver;
+    }
+
+    public void openPortPointHu() {
+        driver.get("https://port.hu/");
+    }
+
+    public void chooseProgramType(PortProgramType portProgramType) {
+        driver.findElement(programType).click();
+        driver.findElement(By.linkText(portProgramType.getValue())).click();
+        driver.findElement(searchProgramButton).click();
+    }
+
+
+
+
+
+
+
+
 }
