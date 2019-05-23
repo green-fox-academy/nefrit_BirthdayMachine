239c2d54afcc33669284dfd03a72a5ecc81d93d7
benciitoo <bence.banszegi@gmail.com>
2019-05-23 14:58:30 +0200

    feat(FacebookProfilePage): get email address method added

--- src/main/java/pages/FacebookProfilePage.java
+++ src/main/java/pages/FacebookProfilePage.java
@@ -3,13 +3,15 @@ package pages;
 import commons.GlobalVariables;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.pagefactory.ByChained;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FacebookProfilePage {
 
     private static final By ABOUT_BUTTON = By.xpath("//*[@id='fbTimelineHeadline']//a[@data-tab-key='about']");
-
+    private static final By EMAIL_ADDRESS = By.xpath("//*[@data-overviewsection='contact_basic']//a[contains(@href,'mailto')]");
+    private String emailAddress;
 
     private WebDriver driver;
     private WebDriverWait wait;
@@ -24,7 +26,10 @@ public class FacebookProfilePage {
         wait.until(ExpectedConditions.elementToBeClickable(ABOUT_BUTTON)).click();
     }
 
-
+    public void getFriendEmailAddress() {
+        emailAddress = wait.until(ExpectedConditions.visibilityOfElementLocated(EMAIL_ADDRESS)).getText();
+        System.out.println(emailAddress);
+    }
 
 
 }
