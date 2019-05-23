48c9879658d5e087cb5beafecd43b06c515f0d0b
benciitoo <bence.banszegi@gmail.com>
2019-05-24 01:07:12 +0200

    refactor(FacebookProfilePage): fixed imports

--- src/main/java/pages/FacebookProfilePage.java
+++ src/main/java/pages/FacebookProfilePage.java
@@ -1,7 +1,11 @@
 package pages;
 
 import commons.GlobalVariables;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
