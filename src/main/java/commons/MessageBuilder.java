f658626b5a14e258846ab95b843453d87109f239
Majoros Aniko <aniko.majoros87@gmail.com>
2019-05-23 18:59:37 +0200

    feat(MessageBuilder): add setGreetingMessage method

--- src/main/java/commons/MessageBuilder.java
+++ src/main/java/commons/MessageBuilder.java
@@ -1,23 +1,27 @@
 package commons;
 
-import org.openqa.selenium.WebDriver;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class MessageBuilder {
 
-    private String greetingMessage = "Hello";
-    private String friendName;
-    private String greetingText;
+    private String greetingMessage;
     private String musicVideoLink;
     private String eventTitle;
     private String restaurantName;
     private String tripPlanUrl;
-    private String weatherBasedDressingTip;
+    private String dressingTip;
+
+    public MessageBuilder(String musicVideoLink, String eventTitle, String restaurantName, String tripPlanUrl, String dressingTip) {
+        this.musicVideoLink = musicVideoLink;
+        this.eventTitle = eventTitle;
+        this.restaurantName = restaurantName;
+        this.tripPlanUrl = tripPlanUrl;
+        this.dressingTip = dressingTip;
+    }
 
     public void setGreetingMessage(){
-        greetingMessage = "";
+        greetingMessage = "Happy birthday, my friend! \n" +
+                "On this special day I would like to cheer you up with this sweet little song :) \n" +
+                musicVideoLink + "\n" +
+                "If You want to go out tonight, ";
     }
 
     public String getGreetingMessage() {
