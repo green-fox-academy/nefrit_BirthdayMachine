431aef63426123033efc00a3598f7ce22abbe962
Majoros Aniko <aniko.majoros87@gmail.com>
2019-05-23 20:20:46 +0200

    refact(GoogleMapsPage): change fields to final

--- src/main/java/pages/GoogleMapsPage.java
+++ src/main/java/pages/GoogleMapsPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import commons.GlobalVariables;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -9,30 +10,37 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GoogleMapsPage {
 
+    private static final String URL = "https://www.google.com/maps";
+    private static final By TRIP_PLANNER_BUTTON = By.id("searchbox-directions");
+    private static final By PUBLIC_TRANSPORT_BUTTON = By.xpath("//*[@id=\"omnibox-directions\"]//div[@data-travel_mode=\"3\"]/button");
+    private static final By DEPARTURE_LOCATION = By.xpath("//*[@id=\"directions-searchbox-0\"]//input");
+    private static final By ARRIVAL_LOCATION = By.xpath("//*[@id=\"directions-searchbox-1\"]//input");
+    private static final By FIRST_RESULT = By.id("section-directions-trip-0");
+    private static final By DETAILS_BUTTON = By.className("section-directions-trip-details-link");
+    private static final By SHARE_LINK_BUTTON = By.className("maps-sprite-pane-action-ic-share-black");
+    private static final By SHARE_LINK_FIELD = By.className("section-copy-link-input");
+
     private WebDriver driver;
     private WebDriverWait wait;
-
-    private String url = "https://www.google.com/maps";
     private String resultUrl;
 
-    private By tripPlannerButton = By.id("searchbox-directions");
-    private By publicTransportButton = By.xpath("//*[@id=\"omnibox-directions\"]//div[@data-travel_mode=\"3\"]/button");
-    private By departureLocation = By.id("directions-searchbox-0");
-    private By arrivalLocation = By.id("directions-searchbox-1");
-    private By firstResult = By.id("section-directions-trip-0");
+    public GoogleMapsPage(WebDriver driver){
+        this.driver = driver;
+        this.wait = new WebDriverWait(this.driver, GlobalVariables.GENERAL_EXPLICIT_TIMEOUT);
+    }
 
     public void getGoogleMapsPage(){
-        driver.get(url);
+        driver.get(URL);
     }
 
     public void getRoutePlannerForm(){
-        wait.until(ExpectedConditions.elementToBeClickable(tripPlannerButton)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(TRIP_PLANNER_BUTTON)).click();
     }
 
     public void planTripWithPublicTransport(String departureLocation, String arrivalLocation){
-        wait.until(ExpectedConditions.elementToBeClickable(publicTransportButton)).click();
-        WebElement departureField = wait.until(ExpectedConditions.visibilityOfElementLocated(this.departureLocation));
-        WebElement arrivalField = wait.until(ExpectedConditions.visibilityOfElementLocated(this.arrivalLocation));
+        wait.until(ExpectedConditions.elementToBeClickable(PUBLIC_TRANSPORT_BUTTON)).click();
+        WebElement departureField = wait.until(ExpectedConditions.visibilityOfElementLocated(this.DEPARTURE_LOCATION));
+        WebElement arrivalField = wait.until(ExpectedConditions.visibilityOfElementLocated(this.ARRIVAL_LOCATION));
         departureField.clear();
         departureField.sendKeys(departureLocation);
         arrivalField.clear();
@@ -40,8 +48,10 @@ public class GoogleMapsPage {
     }
 
     public void setResultUrl(){
-        wait.until(ExpectedConditions.visibilityOfElementLocated(firstResult));
-        resultUrl = driver.getCurrentUrl();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(FIRST_RESULT));
+        wait.until(ExpectedConditions.elementToBeClickable(DETAILS_BUTTON)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(SHARE_LINK_BUTTON)).click();
+        resultUrl = wait.until(ExpectedConditions.visibilityOfElementLocated(SHARE_LINK_FIELD)).getAttribute("value");
     }
 
     public String getResultUrl(){
