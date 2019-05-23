4f90e842483de08c765e1b72bbbcb2ddd9834f05
Majoros Aniko <aniko.majoros87@gmail.com>
2019-05-23 14:48:46 +0200

    feat(GooglePage): add search method, result address getter method

--- src/main/java/pages/GooglePage.java
+++ src/main/java/pages/GooglePage.java
@@ -2,7 +2,9 @@ package pages;
 
 import commons.GlobalVariables;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GooglePage {
@@ -13,9 +15,18 @@ public class GooglePage {
     private String url = "https://www.google.com/";
 
     private By searchField = By.xpath("//*[@id=\"tsf\"]//form//input[@type=\"text\"]");
+    private By resultAddress = By.xpath("//*[@id=\"rhs_block\"]//span[@class=\"LrzXr\"]");
 
     public GooglePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(this.driver, GlobalVariables.GENERAL_EXPLICIT_TIMEOUT);
+    }
+
+    public void searchText(String keywords){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(searchField)).sendKeys(keywords + Keys.ENTER);
+    }
+
+    public String getResultAddress(){
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(resultAddress)).getText();
     }
 }
