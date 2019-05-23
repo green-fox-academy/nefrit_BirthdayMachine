8bb771487a2d3499db2fc5432306e3f725bf8323
benciitoo <bence.banszegi@gmail.com>
2019-05-23 18:22:53 +0200

    fix(AppManager) selectFriendToInteractWith

--- src/main/java/commons/AppManager.java
+++ src/main/java/commons/AppManager.java
@@ -7,17 +7,17 @@ import java.util.List;
 public class AppManager {
 
     public static void selectFriendToInteractWith(WebDriver driver, List<String> listOfFriends) {
-        if (listOfFriends != null) {
-        System.out.println("Friends who have birthday today: ");
-            for (int i = 0; i < listOfFriends.size(); i++) {
-                System.out.println((i + 1) + " -> " + listOfFriends.get(i));
-            }
-            System.out.println("\nSelect your friend by the number above if you want to make her/his (and your) day special.");
-        } else {
+        if (listOfFriends == null || listOfFriends.size() == 0) {
             System.out.println("Sorry, you don't have any friends who has birthday today. Try this app again tomorrow!");
             DriverUtility.quitSession(driver);
             System.exit(0);
         }
+        System.out.println("Friends who have birthday today: ");
+            for (int i = 0; i < listOfFriends.size(); i++) {
+                System.out.println((i + 1) + " -> " + listOfFriends.get(i));
+            }
+            System.out.println("\nSelect your friend by the number above if you want to make her/his (and your) day special:");
+
 
 
 
