e1c085b1de6e90987906c59f36461843782aae87
benciitoo <bence.banszegi@gmail.com>
2019-05-23 17:28:53 +0200

    feat(AppManager) selectFriendToInteractWith

--- src/main/java/commons/AppManager.java
+++ src/main/java/commons/AppManager.java
@@ -1,8 +1,16 @@
 package commons;
 
+import java.util.List;
+
 public class AppManager {
 
+    public static void selectFriendToInteractWith(List<String> listOfFriends) {
+        System.out.println("Friends who have birthday today: ");
+        for (String friend : listOfFriends) {
+            System.out.println(friend);
+        }
 
+    }
 
 
 }
