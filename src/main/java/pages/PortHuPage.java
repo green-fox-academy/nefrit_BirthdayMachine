89d6e75bd7eb487b56c0652874e6e10ded1eb49a
benciitoo <bence.banszegi@gmail.com>
2019-05-24 03:16:30 +0200

    refactor(PortHuPage)

--- src/main/java/pages/PortHuPage.java
+++ src/main/java/pages/PortHuPage.java
@@ -7,19 +7,17 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
-
 public class PortHuPage {
+
+    private static final String PORT_POINT_HU_URL = "https://port.hu/";
+    private static final By PROGRAM_SEARCH_FORM = By.id("top-search-form");
+    private static final By CITY_CHOOSER = By.xpath("//form[@id='top-search-form']/ul/li[2]/a/span");
+    private static final By PROGRAM_TYPE = By.xpath("//form[@id='top-search-form']/ul/li[3]/a/span");
+    private static final By SEARCH_PROGRAM_BUTTON = By.xpath("//form[@id='top-search-form']/button");
+    private static final String DEFAULT_CITY = "Budapest";
 
     private WebDriverWait wait;
     private WebDriver driver;
-    private String portPointHuUrl = "https://port.hu/";
-    private By programSearchFrom = By.id("top-search-form");
-    private By cityChooser = By.xpath("//form[@id='top-search-form']/ul/li[2]/a/span");
-    private By programType = By.xpath("//form[@id='top-search-form']/ul/li[3]/a/span");
-    private By searchProgramButton = By.xpath("//form[@id='top-search-form']/button");
-    private String defaultCity = "Budapest";
-
 
     public PortHuPage(WebDriver driver) {
         this.wait = new WebDriverWait(driver, GlobalVariables.GENERAL_EXPLICIT_TIMEOUT);
@@ -27,20 +25,19 @@ public class PortHuPage {
     }
 
     public void openPortPointHu() {
-        driver.get(portPointHuUrl);
+        driver.get(PORT_POINT_HU_URL);
     }
 
     public void searchForProgram(PortProgramType portProgramType, String city) {
-        driver.findElement(cityChooser).click();
+        driver.findElement(CITY_CHOOSER).click();
         try {
             driver.findElement(By.linkText(city)).click();
 
         } catch (NoSuchElementException e) {
-            driver.findElement(By.linkText(defaultCity)).click();
+            driver.findElement(By.linkText(DEFAULT_CITY)).click();
         }
-        driver.findElement(programType).click();
+        driver.findElement(PROGRAM_TYPE).click();
         driver.findElement(By.linkText(portProgramType.getValue())).click();
-        driver.findElement(searchProgramButton).click();
+        driver.findElement(SEARCH_PROGRAM_BUTTON).click();
     }
 }
-
