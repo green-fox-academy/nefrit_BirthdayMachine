b0da82a5f63c3d4f5ba297ce90c86b48d4a397db
benciitoo <bence.banszegi@gmail.com>
2019-05-24 11:36:50 +0200

    refactor(MessageBuilder): fixed messages

--- src/main/java/commons/MessageBuilder.java
+++ src/main/java/commons/MessageBuilder.java
@@ -20,8 +20,8 @@ public class MessageBuilder {
     public void setGreetingMessage(){
         greetingMessage = "Happy birthday, my friend! " +
                 "On this special day I would like to cheer you up with this sweet little song :) " +
-                musicVideoLink + " If You want to go out tonight, I can suggest You this event: " + eventTitle +
-                " And after that we can have a bite at " + restaurantName + ", and travel there like this: " +
+                musicVideoLink + " . If You want to go out tonight, I can suggest You this event: " + eventTitle +
+                ". And after that we can have a bite at " + restaurantName + ", and travel there like this: " +
                 tripPlanUrl + ". " + dressingSuggestion + " Let me know how you feel about this. :)";
     }
 
@@ -29,7 +29,7 @@ public class MessageBuilder {
         return greetingMessage;
     }
 
-    public String buildDressingSuggestionMessage(int temperature){
+    private String buildDressingSuggestionMessage(int temperature){
         if(temperature < 5){
             return "Bring your winter coat, it will be really cold!";
         } else if(temperature >= 5 && temperature < 15){
